b3124d047b13a7d1dc5aaee9a32902d68b7d5b5d
AS-MI01 <as-mi01@AS-MI01s-Mac-mini.local>
2023-10-02 16:50:46 +0700

    feat: learn common layout, box, column

--- app/src/main/java/com/example/learncompose/MainActivity.kt
+++ app/src/main/java/com/example/learncompose/MainActivity.kt
@@ -5,29 +5,37 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -37,38 +45,42 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import com.example.learncompose.ui.theme.LearnComposeTheme
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.learncompose.ui.theme.LearnComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -95,13 +107,46 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun HomeScreen() {
-    Column(modifier = Modifier.padding(16.dp)) {
-        ImageCompose()
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .fillMaxHeight()
+            .verticalScroll(rememberScrollState())
+    ) {
+        TextCompose()
+//        ImageCompose()
 //        SimpleButton()
 //        RadioButtonCompose()
-        TextFieldCompose()
-        OutlineTextFieldCompose()
+//        TextFieldCompose()
+//        OutlineTextFieldCompose()
+        LayoutCompose()
+        ConstraintLayoutCompose()
     }
+}
+
+// ****************************** Text, Multiple text style
+
+@Composable
+fun TextCompose() {
+    Row() {
+        Text(
+            text = "Hello world!", color = Color.DarkGray, modifier = Modifier.padding(
+                end = 16.dp
+            )
+        )
+        Text(text = buildAnnotatedString {
+            withStyle(style = SpanStyle(color = Color.Red)) {
+                append("Multi")
+            }
+            withStyle(style = SpanStyle(color = Color.Green)) {
+                append("ple")
+            }
+            withStyle(style = SpanStyle(color = Color.Blue)) {
+                append("style")
+            }
+        })
+    }
+
 }
 
 // ****************************** Image
@@ -243,11 +288,13 @@ fun TextFieldCompose() {
     TextField(
         value = text,
         onValueChange = { newText -> text = newText },
+        modifier = Modifier.fillMaxWidth(),
         label = { Text("Name") },
         placeholder = { Text("Enter...") },
         textStyle = TextStyle(
             color = Color.Black, fontWeight = FontWeight.W700, fontSize = 16.sp
         ),
+
         leadingIcon = { Icon(Icons.Default.Person, contentDescription = null) },
         trailingIcon = {
             IconButton(onClick = {
@@ -286,7 +333,7 @@ fun OutlineTextFieldCompose() {
         textStyle = TextStyle(
             color = Color.Black, fontWeight = FontWeight.W700, fontSize = 16.sp
         ),
-        visualTransformation = if(isShowTextValue) VisualTransformation.None else PasswordVisualTransformation(),
+        visualTransformation = if (isShowTextValue) VisualTransformation.None else PasswordVisualTransformation(),
         leadingIcon = { Icon(Icons.Default.Call, contentDescription = null) },
         trailingIcon = {
             IconButton(onClick = {
@@ -301,6 +348,43 @@ fun OutlineTextFieldCompose() {
             imeAction = ImeAction.Done, keyboardType = KeyboardType.Phone
         ),
     )
+}
+
+// ****************************** Layout: Box, Column, Row, Scroll
+@Composable
+fun LayoutCompose() {
+    Column() {
+        OutlineTextFieldCompose()
+        TextFieldCompose()
+    }
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .padding(top = 16.dp)
+            .horizontalScroll(rememberScrollState()),
+    ) {
+        BoxItem(color = Color.Red, modifier = Modifier.clickable {})
+        BoxItem(color = Color.Green)
+        BoxItem(color = Color.Blue)
+        BoxItem(color = Color.Red, modifier = Modifier.clickable {})
+        BoxItem(color = Color.Green)
+        BoxItem(color = Color.Blue)
+    }
+}
+
+@Composable
+fun BoxItem(color: Color = Color.Gray, size: Dp = 100.dp, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .size(size)
+            .background(color = color),
+    ) {}
+
+}
+
+@Composable
+fun ConstraintLayoutCompose() {
+
 }
 
 @Preview(showBackground = true)
